adb7e8eeecc1ae0f8c441d7f2297f8021b1ffb84
Rodrigo Santos <rodrigosc.pi@gmail.com>
2020-08-01 10:18:12 -0300

    Creation of the PaypalController controller

---
+++ app/Controllers/Http/PaypalController.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Config = use('Config');
+const Paypal = use('paypal-rest-sdk');
+
+Paypal.configure(Config.get('paypal.configure'));
+
+class PaypalController {
+  getSuccessURL() {
+    return Config.get('paypal.url_success')
+  }
+
+  getErrorURL() {
+    return Config.get('paypal.urel_error');
+  }
+
+  createPay(payment) {
+    return new Promise((resolve, reject) => {
+      Paypal.payment.create(payment, (err, payment) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(payment);
+        }
+      });
+    });
+  }
+
+  getPay(paymentId) {
+    return new Promise((resolve, reject) => {
+      Paypal.payment.get(paymentId, (err, payment) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(payment);
+        }
+      });
+    });
+  }
+}
+
+module.exports = PaypalController;
